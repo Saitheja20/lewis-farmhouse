3dd189e6be14771a5d4759c2bb6e9d3f8641c216
Saitheja20 <komallasaiteja@gmail.com>
2025-04-08 15:48:35 +0530

    feat: add accommodation, attraction, policies, and barn components
    This commit introduces four new components: AccommodationComponent, AttractionComponent, PoliciesComponent, and BarnComponent. Each component includes a basic template, styling, and a corresponding spec file for testing.

---
+++ src/app/accommodation/accommodation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accommodation',
+  imports: [],
+  templateUrl: './accommodation.component.html',
+  styleUrl: './accommodation.component.css'
+})
+export class AccommodationComponent {
+
+}
---
+++ src/app/barn/barn.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barn',
+  imports: [],
+  templateUrl: './barn.component.html',
+  styleUrl: './barn.component.css'
+})
+export class BarnComponent {
+
+}
