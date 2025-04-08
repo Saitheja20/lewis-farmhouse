5f5176bd7fbbb1a7db93298519f8029eadc18831
Saitheja20 <komallasaiteja@gmail.com>
2025-04-08 17:53:31 +0530

    implement routing and styling for header and footer components
    This commit introduces routing functionality to the application, enabling navigation between different components. It also includes styling enhancements for the header and footer components to improve the user interface.
    
    The following changes were made:
    
    - Configured routing in `app.routes.ts` to map URLs to components like `IndexComponent`, `HeaderComponent`, `BarnComponent`, and `PoliciesComponent`.
    - Updated the header component (`header.component.html`) to use `routerLink` for navigation.
    - Added CSS files (`header.component.css` and `footer.component.css`) to style the header and footer components, including responsive design adjustments.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HeaderComponent } from './header/header.component';
+import { IndexComponent } from './index/index.component';
+import { BarnComponent } from './barn/barn.component';
+ //import { AttractionsComponent } from './attractions/attractions.component';
+ import { PoliciesComponent } from './policies/policies.component';
+export const routes: Routes = [
+  { path: '', component: IndexComponent },
+ // { path: 'about', loadComponent: () => import('./about/about.component').then(m => m.AboutComponent) },
+ // { path: 'contact', loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent) },
+  { path: 'header', component: HeaderComponent },
+  { path: 'barn', component: BarnComponent },
+ // { path: 'attractions', component: AttractionsComponent },
+  {path: 'policies', component: PoliciesComponent},
+ // { path: '**', loadComponent: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent) },
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+i//mport { cz } from '@angular/router';
 
 @Component({
   selector: 'app-header',
